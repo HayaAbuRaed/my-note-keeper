03c33da0ee5ac6643ccc632113562d9665e876fa
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-04-23 01:10:33 +0300

    build the controller get methods

---
+++ Backend/src/controllers/note.contrller.js
@@ -0,0 +1,24 @@
+import Note from "../models/note.model.js";
+
+export const getNotes = async (req, res) => {
+  try {
+    const notes = await Note.find({}); // find({}) => the {} means fined everything (all)
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const note = await Note.findById(id);
+
+    !note && res.status(404).json({ message: "Note not found" });
+
+    note && res.status(200).json(note);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
