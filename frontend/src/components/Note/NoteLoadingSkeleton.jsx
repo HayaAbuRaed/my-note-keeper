17ebd2d7e3ac05ee3f8db1c44f6c559233497b33
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-22 22:40:07 +0300

    build skeletons

---
+++ frontend/src/components/Note/NoteLoadingSkeleton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoadingSkeleton from "../../containers/LoadingSkeleton";
+
+const NoteLoadingSkeleton = () => {
+  return <LoadingSkeleton width={250} height={180} borderRadius="1rem" />;
+};
+
+export default NoteLoadingSkeleton;
--- frontend/src/pages/Home/components/NotesList.jsx
+++ frontend/src/pages/Home/components/NotesList.jsx
@@ -3,16 +3,18 @@ import Note from "../../../components/Note";
 import { NotesContext } from "../../../contexts";
 import { getRandomColor } from "../../../utils";
 import styles from "../styles.module.css";
+import NotesListSkeleton from "./NotesListSkeleton";
 
 const NotesList = () => {
   const { notes, isFetching } = useContext(NotesContext);
 
-  if (isFetching) return <p>Loading...</p>;
+  if (isFetching) return <NotesListSkeleton />;
 
   if (!notes.length) return <p>No notes.</p>;
 
   return (
     <div className={styles.notesContainer}>
+      {/* <NotesListSkeleton /> */}
       {notes.map((note) => (
         <Note key={note._id} note={note} color={getRandomColor()} />
       ))}
