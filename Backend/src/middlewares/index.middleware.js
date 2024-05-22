da3d2633e7330f3b6be773eda11b4f03a1ca0dcc
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-05-23 01:48:35 +0300

    add CORS middleware

--- Backend/src/middlewares/index.middleware.js
+++ Backend/src/middlewares/index.middleware.js
@@ -1,10 +1,12 @@
 import express from "express";
 import noteRoute from "../routes/note.route.js";
+import cors from 'cors';
 
 const setupMiddleware = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  
+  app.use(cors());
+
   // => Importing the routes
   app.use("/notes", noteRoute);
 };
